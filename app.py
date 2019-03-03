eca5766adb9a960d253401d9bda988257d7f8aaf
Flox <florrr1997@gmail.com>
2019-03-03 15:28:02 -0300

    Nuevos cambios en estilo, creo página 404

--- app.py
+++ app.py
@@ -42,7 +42,11 @@ def result():
       result = request.form
       return render_template("home.html",result = result)
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+    
 # Clases (i will refactor this later)
 
 class CodeForm(Form):
