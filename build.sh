b43fa0e80d35cb794982fa6b0d9ae1ff3ba0bf64
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2019-08-10 18:16:05 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-apt update && apt install docker
+apt update
+apt install docker docker-ce docker-ce-cli
 bash ./CleanContainers.sh
 pip3 install -r requirements.txt
 docker build . -t run-container
